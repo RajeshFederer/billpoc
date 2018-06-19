a8a5157fc9277609531ff29e8c29880fb68a14f5
RajeshwaranP-Hexaware <rajeshfederer2@gmail.com>
2018-06-19 15:53:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.use(session({
 }));
 
 
-router.post('/dialogflowAPI', function (req, res) {
+app.post('/chatDialogflowAPI', function (req, res) {
 	var options = {
 		method: 'POST',
-		url: "https://api.api.ai/v1/query?v=20150910",
+		url: 'https://api.api.ai/v1/query?v=20150910',
 		headers: {
 			"Authorization": "Bearer aa3c4bf166e14cdbaa1d46d3a3fbe4b4"
 		},
@@ -36,24 +36,39 @@ router.post('/dialogflowAPI', function (req, res) {
 			res.json({ error: "error in chat server api call" }).end();
 		} else {
 			
-			if(body.result && body.result.action && body.result.action=='discount.discount-custom'){
+			if(body.result && body.result.action && body.result.action=='discount'){
 
-				let resolvedQuery = body.result.resolvedQuery;
-				let count = resolvedQuery.split(',').length;
+				body.result.fulfillment.messages[2].speech = '<a class="pdfClass" data-toggle="modal" data-target="#fundModal">Click here</a> to refer the discount chart for more details'
+				res.json(body).end();
+			} else if(body.result && body.result.action && body.result.action =="riskClass"){
+				
+				let riskClass = body.result.parameters && body.result.parameters.RiskClass ? body.result.parameters.RiskClass : null;
+				console.log('YEAH', riskClass, body.result.parameters);
+				if(riskClass){
 
-				if(count <4){
-					body.result.fulfillment.messages[0].speech = body.result.fulfillment.messages[0].speech.replace('2  coverage', count+' coverage')
-					body.result.fulfillment.messages[2].speech = '<a class="pdfClass" data-toggle="modal" data-target="#fundModal">Click here</a> to refer the discount chart for more details'
+					var options = {
+						method: 'POST',
+						url: "http://10.76.1.53:7999/aa/industry",
+						headers: {},
+						body: {"code": riskClass},
+						json: true
+					};
+					request(options, function (error, response, responseBody) {
+						if (error) {
+							console.log('ERROR IN GUIDEWIRE API CALL');
+							res.json(body).end();
+						} else {
+							console.log('SUCCESS IN GUIDEWIRE API CALL', response, responseBody);
+							if(responseBody && responseBody.description){
+								body.result.fulfillment.messages[0].speech = body.result.fulfillment.messages[0].speech.replace('Candy & Confectionery Products Manufacturing', responseBody.description);
+							}
+							res.json(body).end();
+						}
+					});					
 				} else {
-					body.result.fulfillment.messages[1].speech = "We already offering an exciting <b>20% discount</b> for a minimum of <b>4 coverage</b> sections";
-					body.result.fulfillment.messages.splice(0,1);
-					body.result.fulfillment.messages[1].speech = '<a class="pdfClass" data-toggle="modal" data-target="#fundModal">Click here</a> to refer the discount chart for more details'
+					res.json(body).end();
 				}
-				res.json(body).end();
-			} else if(body.result && body.result.action && body.result.action == "selectDeductible" || body.result && body.result.action && body.result.action == "others"){
-				body.result.fulfillment.messages[0].speech = body.result.fulfillment.messages[0].speech.replace('Click here', '<a>Click here</a>')
-				res.json(body).end();
-      }
+			} 
 			else{
 				res.json(body).end();
 			}
